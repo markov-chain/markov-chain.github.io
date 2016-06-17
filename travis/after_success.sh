43281be6338734676c967e1c7e89fe5126cbadb6
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-17 11:38:01 +0200

    Update Travis’ scripts

--- travis/after_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-if [ ! -z "${RUSTDOC_VERSION}" ] && [ "${RUSTDOC_VERSION}" != "${TRAVIS_RUST_VERSION}" ]; then
-  exit
-fi
-
-curl https://markov-chain.github.io/travis/documentation.sh | bash
